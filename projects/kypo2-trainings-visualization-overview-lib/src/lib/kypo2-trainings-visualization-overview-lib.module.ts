a2d9e6d97d843e7ecaee3e2eb26dd6a750ddf8d5
Hai Duong Tran <445437@mail.muni.cz>
2018-08-20 16:44:42 +0200

    Add d3.js service.

--- projects/kypo2-trainings-visualization-overview-lib/src/lib/kypo2-trainings-visualization-overview-lib.module.ts
+++ projects/kypo2-trainings-visualization-overview-lib/src/lib/kypo2-trainings-visualization-overview-lib.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Kypo2TrainingsVisualizationOverviewLibComponent } from './kypo2-trainings-visualization-overview-lib.component';
+import { D3Service } from 'd3-ng2-service';
 
 @NgModule({
   imports: [
   ],
   declarations: [Kypo2TrainingsVisualizationOverviewLibComponent],
-  exports: [Kypo2TrainingsVisualizationOverviewLibComponent]
+  exports: [Kypo2TrainingsVisualizationOverviewLibComponent],
+  providers: [D3Service]
 })
 export class Kypo2TrainingsVisualizationOverviewLibModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Kypo2TrainingsVisualizationOverviewLibModule } from 'projects/kypo2-trainings-visualization-overview-lib/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Kypo2TrainingsVisualizationOverviewLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
