8e19bcb975980161fb6f59df9267a764bcf4b7f8
Hai Duong Tran <445437@mail.muni.cz>
2018-11-08 14:18:56 +0100

    Add method for registering component into service

--- projects/kypo2-trainings-visualization-overview-lib/src/lib/components/clustering/interfaces/clustering-final-event-service.ts
+++ projects/kypo2-trainings-visualization-overview-lib/src/lib/components/clustering/interfaces/clustering-final-event-service.ts
@@ -1,8 +1,17 @@
 import { PlayerVisualizationData } from './player-visualization-data';
+import { FinalComponent } from '../final/final.component';
 
 export interface ClusteringFinalEventService {
+
+    clusteringFinalComponent: FinalComponent;
+
     clusteringFinalOnPlayerMouseover(player: PlayerVisualizationData): void;
+
     clusteringFinalOnPlayerMousemove(player: PlayerVisualizationData): void;
+
     clusteringFinalOnPlayerMouseout(player: PlayerVisualizationData): void;
+
     clusteringFinalOnPlayerClick(player: PlayerVisualizationData): void;
+
+    registerClusteringFinalComponent(component: FinalComponent): void;
 }
