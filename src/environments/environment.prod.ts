335479feb140e260a0698dcf9ac043fbaa6a1a51
xhasak <456221@mail.muni.cz>
2019-05-02 14:54:06 +0200

    added configuration for implicit flow

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
-  production: true
+  production: true,
+  kypo2TrainingsVisualizationRestBasePath: 'http://147.251.21.216:8085/kypo2-rest-trainings-visualization-overview/api/v1/',
+  defaultPaginationSize: 10,
+
+  // OIDC SETTINGS
+  // Url of the Identity Provider
+  issuer: 'https://oidc.ics.muni.cz/oidc/',
+  // URL of the SPA to redirect the user after silent refresh
+  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin + '/index.html',
+  // The SPA's id. The SPA is registered with this id at the auth-server
+  clientId: '3693320b-6acb-442c-be51-86e18f574f9d',
+  // set the scope for the permissions the client should request
+  scope: 'openid profile email',
+  sessionChecksEnabled: false,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  kypo2TrainingsVisualizationRestBasePath: 'http://147.251.21.216:8085/kypo2-rest-trainings-visualization-overview/api/v1/',
+  defaultPaginationSize: 10,
+
+  // OIDC SETTINGS
+  // Url of the Identity Provider
+  issuer: 'https://oidc.ics.muni.cz/oidc/',
+  // URL of the SPA to redirect the user after silent refresh
+  silentRefreshRedirectUri: window.location.origin,
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin,
+  // The SPA's id. The SPA is registered with this id at the auth-server
+  clientId: '3693320b-6acb-442c-be51-86e18f574f9d',
+  // set the scope for the permissions the client should request
+  scope: 'openid profile email',
+  sessionChecksEnabled: false,
 };
 
 /*
