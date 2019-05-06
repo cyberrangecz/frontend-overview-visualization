e61903e4ed1a3dcc21fb819864f324e0e5f8ab1d
xhasak <456221@mail.muni.cz>
2019-05-06 23:52:56 +0200

    changed configuration according kypo-training

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,15 @@ export const environment = {
 
   // OIDC SETTINGS
   // Url of the Identity Provider
-  issuer: 'https://oidc.ics.muni.cz/oidc/',
+  issuer: 'https://oidc.muni.cz/oidc/',
   // URL of the SPA to redirect the user after silent refresh
-  silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
+  silentRefreshRedirectUri: window.location.origin,
   // URL of the SPA to redirect the user to after login
-  redirectUri: window.location.origin + '/index.html',
-  // The SPA's id. The SPA is registered with this id at the auth-server
-  clientId: '3693320b-6acb-442c-be51-86e18f574f9d',
+  redirectUri: window.location.origin,
+  // The SPA's id. The SPA is registered with this id at the config-server
+  clientId: 'b53f2660-8fa0-4d32-94e4-23a59d7e7077',
   // set the scope for the permissions the client should request
   scope: 'openid profile email',
   sessionChecksEnabled: false,
+
 };
